3adbc7eb6951763429ba5273ecd53d80238b014e
Hourboyloy <chhoysienghour@gmail.com>
2024-09-07 16:31:14 +0700

    update

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
+const sideleft = document.getElementById("sideleft").style;
+const Sideright = document.getElementById("sideright").style;
+const bodystyle = document.getElementById("bodystyle").style;
 
-// let sidenav = document.querySelector("#sidenav")
-// let menu = document.querySelector("#menu")
-// let hidenav = document.querySelector("#hidenav")
-// let rightnavbar = document.querySelector("#rightnavbar")
-// let showContaint1 = document.querySelector("#showContaint1")
-// const showNav =()=>{
-//     menu.style.transition = "all 0.2s ease";
-//     menu.style.display = "none";
-//     sidenav.style.transition = "all 0.6s ease";
-//     sidenav.style.left = "0";
-//     sidenav.style.transition = "all 0.6s ease";
-//     hidenav.style.transition = "all 0.6s ease";
-//     hidenav.style.display = "block";
-//     rightnavbar.style.transition = "all 1s ease";
-//     rightnavbar.style.left = "0";
-// }
+const showNav = () => {
+  sideleft.left = "0";
+  bodystyle.height = "100vh";
+  bodystyle.overflow = "hidden";
+  Sideright.right = "0";
+};
 
-
-// const hideNav =()=>{
-//     rightnavbar.style.transition = "all 0.5s ease";
-//     rightnavbar.style.left = "-100%";
-//     sidenav.style.transition = "all 1.5s ease";
-//     sidenav.style.left = "-100%";
-//     menu.style.display = "block";
-//     hidenav.style.display = "none";
-// }
-
+const closeNav = () => {
+  sideleft.left = "-100%";
+  bodystyle.height = "auto";
+  bodystyle.overflow = "auto";
+  Sideright.right = "-100%";
+};
